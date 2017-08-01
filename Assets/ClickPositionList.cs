bbee868773a38bb329a68aca553ef9bef41818dd
MrElektronix <milovanpelt@gmail.com>
2017-08-01 14:31:44 +0200

    update

---
+++ Assets/ClickPositionList.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ClickPositionList : MonoBehaviour {
+
+    public Transform clickobject;
+    public List<Vector3> CurrentMousePosition = new List<Vector3>();
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetMouseButtonUp(0))
+        {
+            //Debug.Log(Input.mousePosition);
+            Instantiate(clickobject, Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)), transform.rotation);
+            CurrentMousePosition.Add(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)));
+            for (int i = 0; i < CurrentMousePosition.Count; i++)
+            {
+                Debug.Log("currentmouse: " + CurrentMousePosition[i]);
+            }
+        }
+	}
+}
---
+++ Assets/SteeringObject.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteeringObject : MonoBehaviour {
+
+    public Transform target;
+    private float maxSpeed = 15;
+    private float mass = 20;
+
+    private Vector3 position = new Vector3();
+    private Vector3 velocity = new Vector3(2, 0, 0);
+    
+   void Start()
+    {
+        //velocity *= maxSpeed;
+    }
+
+   void Update()
+    {
+        Seek();
+    }
+
+    void Seek()
+    {
+        Vector3 desiredStep = target.position - transform.position;
+        Debug.DrawLine(target.position, transform.position);
+        desiredStep.Normalize();
+
+        Vector3 desiredVelocity = desiredStep * maxSpeed;
+        Vector3 steeringForce = desiredVelocity - velocity;
+
+        velocity += (steeringForce / mass);
+
+        position += (velocity * Time.deltaTime);
+        transform.position = position;
+    }
+}
